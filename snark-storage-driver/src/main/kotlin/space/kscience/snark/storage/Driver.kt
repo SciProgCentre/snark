6e9eb88934c3a9638d6b1b59b1f0c89374036577
Kirill Grachev <grachev.ka@phystech.edu>
2023-04-15 00:01:27 +0300

    Add interface

---
+++ snark-storage-driver/src/main/kotlin/space/kscience/snark/storage/Driver.kt
@@ -0,0 +1,20 @@
+package space.kscience.snark.storage
+
+import java.nio.file.Path
+
+public interface Directory : AutoCloseable {
+    public suspend fun get(filename: String): FileReader?
+
+    public suspend fun create(filename: String, ignoreIfExists: Boolean = false)
+    public suspend fun put(filename: String): FileWriter?
+
+    public suspend fun getSubdir(path: Path): Directory?
+}
+
+public interface FileReader : AutoCloseable {
+    public suspend fun readAll(): ByteArray
+}
+
+public interface FileWriter : AutoCloseable {
+    public suspend fun write(bytes: ByteArray)
+}
