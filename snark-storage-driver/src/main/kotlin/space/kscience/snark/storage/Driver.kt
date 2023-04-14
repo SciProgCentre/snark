a9c0cc10a67aa50b381856a34036d5906f2ebc37
Kirill Grachev <grachev.ka@phystech.edu>
2023-04-15 00:51:45 +0300

    SNRK-48: Remove nullable types

--- snark-storage-driver/src/main/kotlin/space/kscience/snark/storage/Driver.kt
+++ snark-storage-driver/src/main/kotlin/space/kscience/snark/storage/Driver.kt
@@ -3,13 +3,13 @@ package space.kscience.snark.storage
 import java.nio.file.Path
 
 public interface Directory : AutoCloseable {
-    public suspend fun get(filename: String): FileReader?
+    public suspend fun get(filename: String): FileReader
 
     public suspend fun create(filename: String, ignoreIfExists: Boolean = false)
-    public suspend fun put(filename: String): FileWriter?
+    public suspend fun put(filename: String): FileWriter
 
-    public suspend fun getSubdir(path: Path): Directory?
-    public suspend fun createSubdir(dirname: String, ignoreIfExists: Boolean = false): Directory?
+    public suspend fun getSubdir(path: Path): Directory
+    public suspend fun createSubdir(dirname: String, ignoreIfExists: Boolean = false): Directory
 }
 
 public interface FileReader : AutoCloseable {
