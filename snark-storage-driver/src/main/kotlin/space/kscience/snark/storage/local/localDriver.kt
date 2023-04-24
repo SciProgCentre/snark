9651ad1cd9a7052a0dc99fe5c05fd51b89911494
Anton Belyi <anton.8938575@gmail.com>
2023-04-24 15:32:29 +0300

    SNRK-60: slightly changed local driver and added unzip function

--- snark-storage-driver/src/main/kotlin/space/kscience/snark/storage/local/localDriver.kt
+++ snark-storage-driver/src/main/kotlin/space/kscience/snark/storage/local/localDriver.kt
@@ -15,19 +15,19 @@ public class LocalFile(private val path: String) : FileReader, FileWriter {
 
 public class LocalDirectory(private val path: String) : Directory {
     override fun close() {}
-    override suspend fun get(filename: String): FileReader = LocalFile("${this.path}/$filename")
+    override suspend fun get(filename: String): FileReader = LocalFile("${this.path}${File.separator}$filename")
 
     override suspend fun create(filename: String, ignoreIfExists: Boolean) {
-        if (!File(filename).createNewFile() && !ignoreIfExists) {
+        if (!File("${this.path}${File.separator}$filename").createNewFile() && !ignoreIfExists) {
             throw UnsupportedOperationException("File already exists")
         }
     }
 
-    override suspend fun put(filename: String): FileWriter = LocalFile("${this.path}/$filename")
+    override suspend fun put(filename: String): FileWriter = LocalFile("${this.path}${File.separator}$filename")
 
-    override suspend fun getSubdir(dirpath: Path): Directory = LocalDirectory("${this.path}/$dirpath")
+    override suspend fun getSubdir(dirpath: Path): Directory = LocalDirectory("${this.path}${File.separator}$dirpath")
     override suspend fun createSubdir(dirname: String, ignoreIfExists: Boolean): Directory {
-        if (!File(dirname).mkdir() && !ignoreIfExists) {
+        if (!File("${this.path}${File.separator}$dirname").mkdir() && !ignoreIfExists) {
             throw UnsupportedOperationException("File already exists")
         }
         return this.getSubdir(File(dirname).toPath())
