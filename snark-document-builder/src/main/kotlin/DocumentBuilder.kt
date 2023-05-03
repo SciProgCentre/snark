dee40ecdcc699f33fa64e4bc7c92b0251845f264
liubar.pa <liubar.pa@phystech.edu>
2023-05-04 02:33:58 +0300

    SNRK-70: visibility of DEFAULT_DICUMENT_ROOT is specified

--- snark-document-builder/src/main/kotlin/DocumentBuilder.kt
+++ snark-document-builder/src/main/kotlin/DocumentBuilder.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.io.BigDecimalParser
 import space.kscience.snark.storage.*
 import java.nio.file.Path
 
-val DEFAULT_DOCUMENT_ROOT = "main.md"
+private val DEFAULT_DOCUMENT_ROOT = "main.md"
 
 public suspend fun buildDocument(documentDirectory: Directory) {
     val documentRoot = documentDirectory.get(DEFAULT_DOCUMENT_ROOT)
