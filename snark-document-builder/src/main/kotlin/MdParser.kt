e0ef373c485570fea1195f24f96cbc18f1644866
liubar.pa <liubar.pa@phystech.edu>
2023-05-05 00:07:59 +0300

    SNRK-71: parseMd is implemented

--- snark-document-builder/src/main/kotlin/MdParser.kt
+++ snark-document-builder/src/main/kotlin/MdParser.kt
@@ -1,7 +1,16 @@
 package documentBuilder
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+private val MARKDOWN_PARSER = "../nodejs/MarkdownParser.js"
+
 public suspend fun parseMd(mdFile: ByteArray): MdAstRoot {
-    TODO()
+    return jacksonObjectMapper()
+        .readValue<MdAstRoot>(ProcessBuilder("node", MARKDOWN_PARSER, mdFile.toString())
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.INHERIT)
+        .start().inputStream.bufferedReader().readText())
 }
 
 public suspend fun buildDependencyGraphNode(mdFile: ByteArray): DependencyGraphNode {
