5b33419b370b51c86dc4f15c4f650f227742bc1c
Kirill Grachev <grachev.ka@phystech.edu>
2023-05-04 21:11:29 +0300

    SNRK-68: Add methods

--- snark-storage-driver/src/main/kotlin/space/kscience/snark/storage/Driver.kt
+++ snark-storage-driver/src/main/kotlin/space/kscience/snark/storage/Driver.kt
@@ -3,13 +3,27 @@ package space.kscience.snark.storage
 import java.nio.file.Path
 
 public interface Directory : AutoCloseable {
+    @Deprecated(
+        message = "Use Path, not String",
+        level = DeprecationLevel.WARNING,
+    )
     public suspend fun get(filename: String): FileReader
+    // get file from subtree
+    public suspend fun get(filename: Path): FileReader
 
     public suspend fun create(filename: String, ignoreIfExists: Boolean = false)
+    @Deprecated(
+        message = "Use Path, not String",
+        level = DeprecationLevel.WARNING,
+    )
     public suspend fun put(filename: String): FileWriter
+    // put file to subtree
+    public suspend fun put(filename: Path): FileWriter
 
     public suspend fun getSubdir(path: Path): Directory
     public suspend fun createSubdir(dirname: String, ignoreIfExists: Boolean = false): Directory
+
+    public val path: Path
 }
 
 public interface FileReader : AutoCloseable {
