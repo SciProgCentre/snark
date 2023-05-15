f06182ee1d1536db1ac3f1796e6d33646a86dd1f
Kirill Grachev <grachev.ka@phystech.edu>
2023-05-15 18:00:27 +0300

    Add acceptance tests

---
+++ snark-storage-driver/src/test/kotlin/space/kscience/snark/storage/JustCreates.kt
@@ -0,0 +1,25 @@
+package space.kscience.snark.storage
+
+import org.junit.jupiter.api.Test
+import kotlin.io.path.createTempDirectory
+
+class JustCreates {
+
+    @Test
+    fun s3Created() {
+        val dir = Directory.fromConfig(S3ServiceConfig)
+        dir.close()
+    }
+
+    @Test
+    fun s3BucketCreated() {
+        val dir = Directory.fromConfig(S3BucketConfig("snark-test"))
+        dir.close()
+    }
+
+    @Test
+    fun localCreated() {
+        val dir = Directory.fromConfig(LocalConfig(createTempDirectory("snark-test")))
+        dir.close()
+    }
+}
